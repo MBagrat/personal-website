56e8eff17266724e5ac79e7a378b2e491f1ec967
Bagrat Mukaelyan <bagrat.mukaelyan@gmail.com>
2023-01-08 23:53:14 +0400

    implement GDPR and did some refactoring

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,56 @@
+;(function () {
+  <!--@formatter:off-->
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#252e39"
+      },
+      "button": {
+        "background": "#4B7E85"
+      }
+    },
+    "theme": "edgeless",
+    "position": "bottom-left",
+    "type": "opt-in",
+    "content": {
+      "message": "Our website uses cookies to improve your browsing experience. By using our site, you agree to our use of cookies.",
+      "dismiss": "Decline",
+      "allow": "I'm happy with this",
+      "link": "Find out how we use cookies.",
+      "href": "https://mbagrat.com/terms"
+    },
+    onInitialise: function (status) {
+      var type = this.options.type;
+      var didConsent = this.hasConsented();
+      if (type === 'opt-in' && didConsent) {
+        // enable cookies
+        loadGAonConsent();
+      }
+      if (type === 'opt-out' && !didConsent) {
+        // disable cookies
+      }
+    },
+    onStatusChange: function(status, chosenBefore) {
+      var type = this.options.type;
+      var didConsent = this.hasConsented();
+      if (type === 'opt-in' && didConsent) {
+        // enable cookies
+        loadGAonConsent();
+      }
+      if (type === 'opt-out' && !didConsent) {
+        // disable cookies
+      }
+    },
+    onRevokeChoice: function() {
+      var type = this.options.type;
+      if (type === 'opt-in') {
+        // disable cookies
+      }
+      if (type === 'opt-out') {
+        // enable cookies
+        loadGAonConsent();
+      }
+    }
+  });
+  <!--@formatter:on-->
+})();
